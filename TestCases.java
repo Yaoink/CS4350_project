eacdbaa2d041e4ccd762883c2e78238690ba322a
Nancy Quan <51468095+Yaoink@users.noreply.github.com>
2024-10-15 17:31:19 -0400

    comments with methods to test

--- TestCases.java
+++ TestCases.java
@@ -1,10 +1,3 @@
-import org.junit.*;
-import org.junit.runner.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.test.autoconfigure.json.*;
-import org.springframework.boot.test.context.*;
-import org.springframework.boot.test.json.*;
-import org.springframework.test.context.junit4.*;
 
 //for MVC testing
 import static org.assertj.core.api.Assertions.*;
@@ -12,8 +5,29 @@ import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+/*methods to test
+    - if user has account already and lets them know they already have an account when they try to signup 
+    - if database successfully saves user account
+    - if account is created in signup
+    - if password is generated randomly based on specifications
+        -lower case
+        -upper case
+        -symbols
+    - if password length is met
+    - if saved password database connects to the front end
+    - if saved passwords displays
+    - if forgot link is sent to email associated with account
+    - if password is reset after user changes it
+    - if webapp connects to browswer and loads the homepage
+    - if user can edit a saved password
+    - if a saved password updates after user edits it
+    - if user can add a website and an existing password
+    - if user can logout
+*/
+
 public class TestCases {
-    public void sample{
+    public void sample
+    {
 
     }
 }
