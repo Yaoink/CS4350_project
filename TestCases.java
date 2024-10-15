ab51f300d2a447eef696d44e677b0df4858b7cd4
Nancy Quan <51468095+Yaoink@users.noreply.github.com>
2024-10-15 14:28:17 -0400

    importing the libraries needed

---
+++ TestCases.java
@@ -0,0 +1,19 @@
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.autoconfigure.json.*;
+import org.springframework.boot.test.context.*;
+import org.springframework.boot.test.json.*;
+import org.springframework.test.context.junit4.*;
+
+//for MVC testing
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class TestCases {
+    public void sample{
+
+    }
+}
